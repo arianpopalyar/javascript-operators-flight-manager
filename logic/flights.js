1540287bc3d8951f3245ca0dcbf3866142fd4737
arianpopalyar <arian_popalyar@yahoo.com>
2022-05-20 22:49:10 +0100

    adding requested changes

--- logic/flights.js
+++ logic/flights.js
@@ -1,25 +1,18 @@
 function Flights() {
- function calculateNumberOfFlights(passengers, capacity){
-     let flights;
-     try {
-         if(passengers % capacity === 0) {
-            flights = passengers/capacity;
-         }
-        
+    function calculateNumberOfFlights(passengers, capacity){
+        let flights;
         if((passengers < 0) || (!Number.isInteger
             (Number(passengers)))){
             throw new Error("The number of passengers must be a positive integer value");
         }
         if(capacity<0 || !Number.isInteger(Number(capacity))){
-        throw new Error("The capacity of the flight must be a positive integer value");
+            throw new Error("The capacity of the flight must be a positive integer value");
         }
-    } 
-    catch(error) {
-        console.log(error);
-    }
-    finally {
-        console.log(passengers, capacity);
-        // codes that gets executed anyway
+        if(passengers % capacity === 0) {
+            flights = passengers/capacity;
+        } else {
+            flights = math.floor(passengers/capacity) + 1;
+        }
     }
 
     function checkAircraftRevision(distanceLimit, distances) {
--- logic/frontend.js
+++ logic/frontend.js
@@ -13,6 +13,9 @@ function onCalculateNumberOfFlights() {
     } catch (error) {
         document.getElementById('flights').innerHTML = error; 
     } 
+    finally {
+        console.log("You inseted" + passengesrs+ "passenger and" +capacity+"as Capacity")
+    }
 }
 
 function onCalculateTotalFinalPrice(object) {
