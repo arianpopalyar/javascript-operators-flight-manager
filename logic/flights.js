96a8a40ce02dc002fa1bdcafa975388dd736d67c
arianpopalyar <arian_popalyar@yahoo.com>
2022-05-20 16:29:25 +0100

    adding inital tasks

--- logic/flights.js
+++ logic/flights.js
@@ -1,4 +1,28 @@
 function Flights() {
+ function calculateNumberOfFlights(numberOfPasenger, flightCapasity){
+     
+     try {
+        numberOfFlights = numberOfPasenger/flightCapasity;
+        if(numberOfPasenger<0){
+            throw "The number of passengers must be a positive integer value";
+        }
+        if(flightCapasity<0){
+        throw "The capacity of the flight must be a positive integer value"
+        }
+    } 
+    catch(error) {
+        console.log(error);
+    }
+    finally {
+        console.log(numberOfPasenger, flightCapasity);
+        // codes that gets executed anyway
+    }
 
+    function checkAircraftRevision(distenceLimit, distences[]) {
+        
+
+    }
+
+ }return {calculateNumberOfFlights, checkAircraftRevision}; 
 }
-
+module.exports = Flights();
