7a57535ca62ace84ec6ed8c358cb33a26878465b
arianpopalyar <arian_popalyar@yahoo.com>
2022-05-20 21:26:03 +0100

    adding the state 3

--- logic/flights.js
+++ logic/flights.js
@@ -18,9 +18,26 @@ function Flights() {
         // codes that gets executed anyway
     }
 
-    function checkAircraftRevision(distenceLimit, distences[]) {
+    function checkAircraftRevision(distanceLimit, distances) {
+        let totalDistance = distances.reduce(sumTotalDistance, 0);
         
+        function sumTotalDistance(total, num){
+            return total + num
+        }
 
+        let distanceLimit
+        if(totalDistance <= distanceLimit/2){
+            return "The revision needs to be done within the next 3 months";
+        }
+        else if(totalDistance > distanceLimit/2 || totalDistance < distanceLimit * 3/4){
+            return "The revision needs to be done within the next 2 months";
+        }
+        else if(totalDistance > distanceLimit*3/4 && totalDistance<=distanceLimit){
+            return "The revision needs to be done within the next month";
+        }
+        else if(totalDistance >distanceLimit){
+            throw (error)
+        }
     }
 
  }return {calculateNumberOfFlights, checkAircraftRevision}; 
