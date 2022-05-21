7d02599273afccc2b72d54a70c6eae4f235dd1b3
arianpopalyar <arian_popalyar@yahoo.com>
2022-05-21 21:19:55 +0100

    adding the changes

--- logic/flights.js
+++ logic/flights.js
@@ -1,22 +1,26 @@
 function Flights() {
     function calculateNumberOfFlights(passengers, capacity){
         let flights;
+
         if((passengers < 0) || (!Number.isInteger
             (Number(passengers)))){
             throw new Error("The number of passengers must be a positive integer value");
         }
-        if(capacity<0 || !Number.isInteger(Number(capacity))){
+        if((capacity < 0)|| (!Number.isInteger(Number(capacity)))){
             throw new Error("The capacity of the flight must be a positive integer value");
         }
-        if(passengers % capacity === 0) {
+        if(passengers % capacity == 0) {
             flights = passengers/capacity;
         } else {
             flights = math.floor(passengers/capacity) + 1;
         }
+        return flights;
     }
 
-    function checkAircraftRevision(distanceLimit, distances) {
-        let totalDistance = distances.reduce(sumTotalDistance, 0);
+    function checkAircraftRevision(distanceLimit, distancesArray) {
+        let totalDistance =0;
+        let distance;
+        totalDistance = distancesArray.reduce(sumTotalDistance, 0);
         
         function sumTotalDistance(total, num){
             return total + num
