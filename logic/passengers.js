58be766fa67f39ff594e8b4e5a562eb394e6db5b
arianpopalyar <arian_popalyar@yahoo.com>
2022-05-31 19:20:00 +0100

    complete task 7

--- logic/passengers.js
+++ logic/passengers.js
@@ -14,9 +14,62 @@ function Passengers() {
         }
         
     }
-    function distributeAllSeatsToAllPassengers(vipPassengers, regularPassengers, flights,
-        businessSeatsPerFlight, economySeatsPerFlight){
+    function distributeAllSeatsToAllPassengers(vipPassengers, regularPassengers, nrOfFlights,
+        businessSeatsPerFlight, economySeatsPerFlight) {
+            let vipPassengersWithBusinessSeats=0, vipPassengerWithEconomySeats=0,
+            regularPassengersWithBusinessSeats=0, regularPassengersWithEconomySeats=0;
+            let availableBusinessSeats = nrOfFlights * businessSeatsPerFlight;
+            let availableEconomySeats = nrOfFlights * economySeatsPerFlight;
 
+            var vipBusinessConfiguration = {passengers:vipPassengers, seats:availableBusinessSeats};
+            vipPassengersWithBusinessSeats = updateConfiguration(vipBusinessConfiguration,
+                businessSeatsPerFlight);
+            
+            var vipEconomyConfiguration = {passengers:vipBusinessConfiguration.passengers,
+            seats:availableEconomySeats};
+            vipPassengerWithEconomySeats = updateConfiguration(vipEconomyConfiguration, economySeatsPerFlight);    
+
+            var regularBusinessConfiguration ={passengers:regularPassengers,
+            seats:vipBusinessConfiguration.seats};
+            regularPassengersWithBusinessSeats = updateConfiguration(regularBusinessConfiguration,
+                businessSeatsPerFlight);
+        var regularEconomyConfiguration = {passengers:regularBusinessConfiguration.passengers,
+            seats:vipEconomyConfiguration.seats};
+        regularPassengersWithEconomySeats = updateConfiguration(regularEconomyConfiguration,
+            economySeatsPerFlight);
+        return {vipPassengersWithBusinessSeats:vipPassengersWithBusinessSeats,
+        vipPassengerWithEconomySeats:vipPassengerWithEconomySeats,
+    regularPassengersWithBusinessSeats:regularPassengersWithBusinessSeats,
+    regularPassengersWithEconomySeats:regularPassengersWithEconomySeats
+}
+    }   
+
+
+    function updateConfiguration(configuration, seatsPerFlight){
+        let passengersWithSeats = 0;
+
+        while(configuration.passengers > 0) {
+            if(configuration.seats > 0) {
+                if(configuration.passengers >= configuration.seats){
+                    if(configuration.seats > configuration.seatsPerFlight) {
+                        configuration.passengers -= seatsPerFlight;
+                        configuration.seats -= seatsPerFlight;
+                        passengersWithSeats += seatsPerFlight;
+                    } else {
+                        configuration.passengers -= configuration.seats;
+                        passengersWithSeats += configuration.seats;
+                        configuration.seats = 0;
+                    }
+                } else{
+                    passengersWithSeats += configuration.passengers;
+                    configuration.seats -= configuration.passengers;
+                    configuration.seats =0;
+                }
+            } else {
+                break;
+            }
+        }
+        return passengersWithSeats;
     }
 
     function calculateTotalNumberOfPassengers(){
--- logic/util.js
+++ logic/util.js
@@ -1,3 +1,23 @@
 function Util() {
+    function calculateTotalDistributedPassengers(distributedPassengers) {
+        let totalDistributedPassengers = 0;
+        let value;
+
+        for(value in distributedPassengers) {
+            totalDistributedPassengers += distributedPassengers[value];
+        }
+    }
+
+    function calculateTotalNumberOfPassengers(passengersArray){
+        let totalPassengers = 0;
+        let passenger =0;
+        for(passenger of passengersArray){
+            totalPassengers += passenger;
+        }
+        return totalPassengers;
+    }
+
+    return {calculateTotalDistributedPassengers, calculateTotalNumberOfPassengers}
 }
+module.exports = Util();
 
